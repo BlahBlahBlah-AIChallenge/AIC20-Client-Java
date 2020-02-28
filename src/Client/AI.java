1de6ca1f318c25aac1640516075797fd85a6a121
XamarinDeveloper <xamarin.cs@gmail.com>
2020-02-29 02:09:15 +0330

    Add doSpell

--- src/Client/AI.java
+++ src/Client/AI.java
@@ -129,6 +129,10 @@ public class AI {
 
     }
 
+    public void doSpell(){
+
+    }
+
     public void end(World world, Map<Integer, Integer> scores) {
         System.out.println("end started");
         System.out.println("My score: " + scores.get(world.getMe().getPlayerId()));
